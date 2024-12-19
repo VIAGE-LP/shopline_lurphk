9b6eeb678076e11bcee318eff41d8707c0469fcb
Pickachu Chen <pickachu1006@gmail.com>
2024-12-19 15:57:25 +0800

    20241219_member_5

--- lurphkmember/js/all.js
+++ lurphkmember/js/all.js
@@ -88,7 +88,7 @@ $(document).ready(function () {
     if (edmhref.includes("#member")) {
         if (window.innerWidth < 768) {
             $('html , body').animate({
-                scrollTop: $('#member_box').offset().top - 50,
+                scrollTop: $('#member_box').offset().top - 40,
             }, 900);
             setTimeout(() => {
                 $('html , body').animate({
