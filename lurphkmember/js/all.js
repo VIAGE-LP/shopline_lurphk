6cea8640b239786984a67c7a25fad0a9272dbe75
Pickachu Chen <pickachu1006@gmail.com>
2025-02-14 15:11:42 +0800

    20250214_add member popup

--- lurphkmember/js/all.js
+++ lurphkmember/js/all.js
@@ -75,21 +75,54 @@ $(document).ready(function () {
 
 
     //member
-    if (edmhref.includes("#member_Br")) {
+    if(edmhref.includes("#member_Bronze")){
         $(".member_Br").show();
-        $(".member_use").show();
+        // $(".member_use").show();
+
+        //member_popup 視窗 open
+        $(".discount").text("獨家網頁優惠");
+        $(".medal_text_1").text("銅");
+        $(".medal_text_2").text("網頁");
+        $("#member_popup").show();
+        setTimeout(function() {
+            $(".progress-marker").addClass("moveto_silver");
+        }, 500);
     } else if (edmhref.includes("#member_S200")) {
         $(".member_S200").show();
         $(".member_use").show();
+
+        member_scroll()
+
+        //member_popup 視窗 open
+        $(".discount").text("立即折 200 元！");
+        $(".medal_text_1").text("銀");
+        $(".medal_text_2").text("銀級");
+        $("#member_popup").show();
+        setTimeout(function() {
+            $(".progress-marker").addClass("moveto_gold");
+        }, 500);
     } else if (edmhref.includes("#member_G450")) {
         $(".member_G450").show();
         $(".member_use").show();
+
+        member_scroll()
+
+        //member_popup 視窗 open
+        $(".discount").text("立即折 450 元！");
+        $(".medal_text_1").text("金");
+        $(".medal_text_2").text("金級");
+        $("#member_popup").show();
+        setTimeout(function() {
+            $(".progress-marker").addClass("moveto_bkgold");
+        }, 500);
     } else if (edmhref.includes("#member_G600")) {
         $(".member_G600").show();
         $(".member_use").show();
+
+        member_scroll()
     }
 
-    if (edmhref.includes("#member")) {
+    function member_scroll(){
         if (window.innerWidth < 768) {
             $('html , body').animate({
                 scrollTop: $('#member_box').offset().top - 40,
@@ -115,6 +148,13 @@ $(document).ready(function () {
         navigator.clipboard.writeText(couponcode);
         confirm(`恭喜您符合會員等級-${level}級！\n您已複製以下優惠代碼：${couponcode}\n選擇商品加入購物車後，於「優惠代碼」欄位輸入：${couponcode}\n即可享有${level}級會員限定優惠=網頁價格再折台幣${money}!!`)
     }
+
+    $(".member_popup_copy .member_S200").on("click", function () {
+        copybtn("銀", "LNMBS200", "200元(≒HKD$48)");
+    });
+    $(".member_popup_copy .member_G450").on("click", function () {
+        copybtn("金", "LNMBG450", "450元(≒HKD$107)");
+    });
 
     $(".member_S200").on("click", function () {
         copybtn("銀", "LNMBS200", "200元(≒HKD$48)");
